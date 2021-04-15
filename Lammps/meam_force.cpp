9ab351c05c5da428cb52e4bc9040cbfee3865a78
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-04-15 09:48:32 +0200

    update ddrho2

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -193,6 +193,7 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
             }
             arg = delij[n] * delij[p] * this->v2D[nv2];
             arg1i2 = arg1i2 + arho2[i][nv2] * arg;
+            arg1i2_d = arg1i2 + darho2dr[i][nv2] * arg;
             arg1j2 = arg1j2 + arho2[j][nv2] * arg;
             nv2 = nv2 + 1;
           }
@@ -227,6 +228,12 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         //     rho2 terms
         a2 = 2 * sij / rij2;
         drho2dr1 = a2 * (drhoa2j - 2 * rhoa2j / rij) * arg1i2 - 2.0 / 3.0 * arho2b[i] * drhoa2j * sij; //--- 4.30(d): arho2b is W_{2i}
+        drho2dr1 = a2 *                (1)           *   (2)  -              (3)
+        ddrho2ddr1 = a2 * (ddr(1)*2+(1)*ddr(2))-ddr(3); //--- 4.30(d): arho2b is W_{2i}
+        ddr(1) = ddrhoa2j-2*(rhoa2j/rij+drhoa2j)/rij
+        ddr(2) = arg1i2_d
+        ddr(3) = 2.0 / 3.0 * (darho2b[i] * drhoa2j+arho2b[i]*ddrhoa2j) * sij //--- define darho2b[i] and ddrhoa2j
+          
         drho2dr2 = a2 * (drhoa2i - 2 * rhoa2i / rij) * arg1j2 - 2.0 / 3.0 * arho2b[j] * drhoa2i * sij;
         a2 = 4 * sij / rij2;
         for (m = 0; m < 3; m++) {
