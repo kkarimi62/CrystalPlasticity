c8063dcff5404a82293053cb020b0d1cef926794
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-04-27 00:26:45 +0200

    Update pair_meamc.cpp

--- Lammps/pair_meamc.cpp
+++ Lammps/pair_meamc.cpp
@@ -651,7 +651,6 @@ int PairMEAMC::pack_forward_comm(int n, int *list, double *buf,
                                 int /*pbc_flag*/, int * /*pbc*/)
 {
   int i,j,k,m;
-  cout << "hello from PairMEAMC::pack_forward_comm\n";
 
   m = 0;
   for (i = 0; i < n; i++) {
@@ -704,7 +703,6 @@ int PairMEAMC::pack_forward_comm(int n, int *list, double *buf,
 void PairMEAMC::unpack_forward_comm(int n, int first, double *buf)
 {
   int i,k,m,last;
-  cout << "hello from PairMEAMC::unpack_forward_comm\n";
 
   m = 0;
   last = first + n;
@@ -755,7 +753,6 @@ void PairMEAMC::unpack_forward_comm(int n, int first, double *buf)
 int PairMEAMC::pack_reverse_comm(int n, int first, double *buf)
 {
   int i,k,m,last;
-  cout << "hello from PairMEAMC::pack_reverse_comm\n";
 
   m = 0;
   last = first + n;
@@ -797,7 +794,6 @@ int PairMEAMC::pack_reverse_comm(int n, int first, double *buf)
 void PairMEAMC::unpack_reverse_comm(int n, int *list, double *buf)
 {
   int i,j,k,m;
-  cout << "hello from PairMEAMC::unpack_reverse_comm\n";
 
   m = 0;
   for (i = 0; i < n; i++) {
@@ -838,7 +834,6 @@ void PairMEAMC::unpack_reverse_comm(int n, int *list, double *buf)
 
 double PairMEAMC::memory_usage()
 {
-  cout << "hello from PairMEAMC::memory_usage\n";
   double bytes = 11 * meam_inst->nmax * sizeof(double);
   bytes += (3 + 6 + 10 + 3 + 3 + 3) * meam_inst->nmax * sizeof(double);
   bytes += 3 * meam_inst->maxneigh * sizeof(double);
