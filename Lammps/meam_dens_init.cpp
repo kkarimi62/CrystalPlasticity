d45fff2b9e695230437c7f9e20f094c87fdd341e
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-10 18:47:31 +0200

    Update meam_dens_init.cpp

--- Lammps/meam_dens_init.cpp
+++ Lammps/meam_dens_init.cpp
@@ -297,7 +297,7 @@ MEAM::getscreen(int i, double* scrfcn, double* dscrfcn, double* ddscrfcn, double
       arg1 = dscrfcn[jn] * rij;
       dsij = sij * arg1;
       ddsij = dsij * arg1 + sij * arg1_d;
-      dscrfcn[jn] = dscrfcn[jn] * coef1 - coef2; //--- (4.22a) / rij
+      dscrfcn[jn] = dscrfcn[jn] * coef1 - coef2; //--- (4.22a) / rij: units of s/r^2
       ddscrfcn[jn] = - drinv * dfc * dsij + fcij * ddsij - drinv * ( dsij * dfc - sij * ddfc * drinv ); //--- units of s/r^2
     }
 
