8218f60d0745b9c5a3422013a377bfd5e000b3d1
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-11 01:10:38 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -66,6 +66,7 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
   double ddrho3drmdrn1[6], ddrho3drmdrn2[6], ddrhodrmdrn1[6], ddrhodrmdrn2[6];
   double ddUdrdrijm[3], ddUdrijmdrijn[6];
   double stiff, stiff0, stiff1, stiff2;
+  double stif1, stif2;
   double n0, n1, n2;
   double m0, m1, m2;
   double ddt1drdr1,  ddt2drdr1,  ddt3drdr1;
@@ -1130,14 +1131,14 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
             //--- add stiffness
             if (!iszero(dsij1) || !iszero(dsij2) || !iszero(ddsij1) || !iszero(ddsij2) ) {
             
-            stiff1 = ddUddsij * dsij1 * dsij1 * rik2 +
-                     ddUdrijds * 2.0 * dsij1 * rik+
+            stif1 = ddUddsij * dsij1 * dsij1 * rik2 +
+                     ddUdrijds * 2.0 * dsij1 * rik +
                      dUdsij * ( ddsij1 - dsij1); //--- units of u/r^2
-            stiff1 *= rik2; //--- units of energy
-            stiff2 = ddUddsij * dsij2 * dsij2 * rjk2 +
+            stif1 *= rik2; //--- units of energy
+            stif2 = ddUddsij * dsij2 * dsij2 * rjk2 +
                      ddUdrijds * 2.0 * dsij2 * rjk+
                      dUdsij * ( ddsij2 - dsij2 );
-             stiff2 *= rjk2;
+             stif2 *= rjk2;
 
           //--- per-atom modulus
 //            n0 = dxik / rik;
@@ -1146,32 +1147,32 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
 //            m0 = dxjk / rjk;
 //            m1 = dyjk / rjk;
 //            m2 = dzjk / rjk;
-//           vm[ 0 ]  = -third * (stiff1 * n0 * n0 * n0 * n0+stiff2 * m0 * m0 * m0 * m0);
-//           vm[ 1 ]  = -third * (stiff1 * n0 * n0 * n1 * n1+stiff2 * m0 * m0 * m1 * m1);
-//           vm[ 2 ]  = -third * (stiff1 * n0 * n0 * n2 * n2+stiff2 * m0 * m0 * m2 * m2);
-//           vm[ 3 ]  = -third * (stiff1 * n0 * n0 * n0 * n1+stiff2 * m0 * m0 * m0 * m1);
-//           vm[ 4 ]  = -third * (stiff1 * n0 * n0 * n0 * n2+stiff2 * m0 * m0 * m0 * m2);
-//           vm[ 5 ]  = -third * (stiff1 * n0 * n0 * n1 * n2+stiff2 * m0 * m0 * m1 * m2);
+//           vm[ 0 ]  = -third * (stif1 * n0 * n0 * n0 * n0+stif2 * m0 * m0 * m0 * m0);
+//           vm[ 1 ]  = -third * (stif1 * n0 * n0 * n1 * n1+stif2 * m0 * m0 * m1 * m1);
+//           vm[ 2 ]  = -third * (stif1 * n0 * n0 * n2 * n2+stif2 * m0 * m0 * m2 * m2);
+//           vm[ 3 ]  = -third * (stif1 * n0 * n0 * n0 * n1+stif2 * m0 * m0 * m0 * m1);
+//           vm[ 4 ]  = -third * (stif1 * n0 * n0 * n0 * n2+stif2 * m0 * m0 * m0 * m2);
+//           vm[ 5 ]  = -third * (stif1 * n0 * n0 * n1 * n2+stif2 * m0 * m0 * m1 * m2);
 //           //
-//           vm[ 6 ]  = -third * (stiff1 * n1 * n1 * n1 * n1+stiff2 * m1 * m1 * m1 * m1);
-//           vm[ 7 ]  = -third * (stiff1 * n1 * n1 * n2 * n2+stiff2 * m1 * m1 * m2 * m2);
-//           vm[ 8 ]  = -third * (stiff1 * n1 * n1 * n0 * n1+stiff2 * m1 * m1 * m0 * m1);
-//           vm[ 9 ]  = -third * (stiff1 * n1 * n1 * n0 * n2+stiff2 * m1 * m1 * m0 * m2);
-//           vm[ 10 ] = -third * (stiff1 * n1 * n1 * n1 * n2+stiff2 * m1 * m1 * m1 * m2);
+//           vm[ 6 ]  = -third * (stif1 * n1 * n1 * n1 * n1+stif2 * m1 * m1 * m1 * m1);
+//           vm[ 7 ]  = -third * (stif1 * n1 * n1 * n2 * n2+stif2 * m1 * m1 * m2 * m2);
+//           vm[ 8 ]  = -third * (stif1 * n1 * n1 * n0 * n1+stif2 * m1 * m1 * m0 * m1);
+//           vm[ 9 ]  = -third * (stif1 * n1 * n1 * n0 * n2+stif2 * m1 * m1 * m0 * m2);
+//           vm[ 10 ] = -third * (stif1 * n1 * n1 * n1 * n2+stif2 * m1 * m1 * m1 * m2);
 //           //
-//           vm[ 11 ] = -third * (stiff1 * n2 * n2 * n2 * n2+stiff2 * m2 * m2 * m2 * m2);
-//           vm[ 12 ] = -third * (stiff1 * n2 * n2 * n0 * n1+stiff2 * m2 * m2 * m0 * m1);
-//           vm[ 13 ] = -third * (stiff1 * n2 * n2 * n0 * n2+stiff2 * m2 * m2 * m0 * m2);
-//           vm[ 14 ] = -third * (stiff1 * n2 * n2 * n1 * n2+stiff2 * m2 * m2 * m1 * m2);
+//           vm[ 11 ] = -third * (stif1 * n2 * n2 * n2 * n2+stif2 * m2 * m2 * m2 * m2);
+//           vm[ 12 ] = -third * (stif1 * n2 * n2 * n0 * n1+stif2 * m2 * m2 * m0 * m1);
+//           vm[ 13 ] = -third * (stif1 * n2 * n2 * n0 * n2+stif2 * m2 * m2 * m0 * m2);
+//           vm[ 14 ] = -third * (stif1 * n2 * n2 * n1 * n2+stif2 * m2 * m2 * m1 * m2);
 //           //
-//           vm[ 15 ] = -third * (stiff1 * n0 * n1 * n0 * n1+stiff2 * m0 * m1 * m0 * m1);
-//           vm[ 16 ] = -third * (stiff1 * n0 * n1 * n0 * n2+stiff2 * m0 * m1 * m0 * m2);
-//           vm[ 17 ] = -third * (stiff1 * n0 * n1 * n1 * n2+stiff2 * m0 * m1 * m1 * m2);
+//           vm[ 15 ] = -third * (stif1 * n0 * n1 * n0 * n1+stif2 * m0 * m1 * m0 * m1);
+//           vm[ 16 ] = -third * (stif1 * n0 * n1 * n0 * n2+stif2 * m0 * m1 * m0 * m2);
+//           vm[ 17 ] = -third * (stif1 * n0 * n1 * n1 * n2+stif2 * m0 * m1 * m1 * m2);
 //           //
-//           vm[ 18 ] = -third * (stiff1 * n0 * n2 * n0 * n2+stiff2 * m0 * m2 * m0 * m2);
-//           vm[ 19 ] = -third * (stiff1 * n0 * n2 * n1 * n2+stiff2 * m0 * m2 * m1 * m2);
+//           vm[ 18 ] = -third * (stif1 * n0 * n2 * n0 * n2+stif2 * m0 * m2 * m0 * m2);
+//           vm[ 19 ] = -third * (stif1 * n0 * n2 * n1 * n2+stif2 * m0 * m2 * m1 * m2);
 //           //
-//           vm[ 20 ] = -third * (stiff1 * n1 * n2 * n1 * n2+stiff2 * m1 * m2 * m1 * m2);
+//           vm[ 20 ] = -third * (stif1 * n1 * n2 * n1 * n2+stif2 * m1 * m2 * m1 * m2);
 //           //
 //           nv3 = 0;
 //           nv2 = 6;
