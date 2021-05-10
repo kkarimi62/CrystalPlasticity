8ff2cb17fe42571f96b866e7776ad6509c117561
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-10 22:54:07 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -946,14 +946,16 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         
         
         //--- sij contribution ?????????????????
-        if (!iszero(dscrfcn[fnoffset + jn]) or !iszero(ddscrfcn[fnoffset + jn])){
-          stiff += ddUddsij * dscrfcn[fnoffset + jn] * dscrfcn[fnoffset + jn]+// * rij2 +
-                   ddUdrijds * 2.0 * dscrfcn[fnoffset + jn]+// * rij+
-                   dUdsij * ( ddscrfcn[fnoffset + jn] - dscrfcn[fnoffset + jn]*recip);
-          stiff0 = 0.0;
-          for (m = 0; m < 3; m++) stiff0 += ddUdrijmds[m] * delij[m];
-          stiff0 *= 2.0 * dscrfcn[fnoffset + jn]*recip;
-          stiff +=  stiff0;
+        stiff0 = 0.0;
+        if (!iszero(dscrfcn[fnoffset + jn])) {
+          stiff += ddUddsij * dscrfcn[fnoffset + jn] * dscrfcn[fnoffset + jn] +
+                   ddUdrijds * 2.0 * dscrfcn[fnoffset + jn] +
+                   dUdsij * ( ddscrfcn[fnoffset + jn] - dscrfcn[fnoffset + jn] * recip );
+          for (m = 0; m < 3; m++) {
+            stiff0 += ddUdrijmds[m] * delij[m];
+          }
+          stiff0 *= 2.0 * dscrfcn[fnoffset + jn] * recip;
+          stiff +=  stiff0 + stiff1;
         }
         
         
