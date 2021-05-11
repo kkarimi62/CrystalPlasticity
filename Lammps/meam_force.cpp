c643624a2134820e36a0ccaae64072d47a817630
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-11 12:30:08 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -1089,9 +1089,9 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
               force1 = dUdsij * dsij1;
               force2 = dUdsij * dsij2;
             //--- add stiffness
-              stif1 =  ddUddsij * dsij1 * dsij1 * rik2 + ddUdrijds * 2.0 * dsij1 * rik;// + dUdsij * ( ddsij1 - dsij1 ); //--- units of u/r^2
+              stif1 =  ddUddsij * dsij1 * dsij1 * rik2 + ddUdrijds * 2.0 * dsij1 * rik + dUdsij;// * ( ddsij1 - dsij1 ); //--- units of u/r^2
 //              stif1 *= rik2; //--- units of energy
-              stif2 =  ddUddsij * dsij2 * dsij2 * rjk2 + ddUdrijds * 2.0 * dsij2 * rjk;// + dUdsij * ( ddsij2 - dsij2 );
+              stif2 =  ddUddsij * dsij2 * dsij2 * rjk2 + ddUdrijds * 2.0 * dsij2 * rjk + dUdsij;// * ( ddsij2 - dsij2 );
 //              stif2 *= rjk2;
 //             cout <<  stif1 << "\t" << stif2 << "\n";
               //
