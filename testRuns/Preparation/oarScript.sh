6358b4e1b916fc19859d6c51cf6de482b8825e8e
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-13 15:43:44 +0200

    update

--- testRuns/Preparation/oarScript.sh
+++ testRuns/Preparation/oarScript.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 EXEC_DIR=/home/kamran.karimi1/Project/git/CrystalPlasticity/lammps-29Oct20/src
+MEAM_library_DIR=/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles/
 
 module load mpich/3.2.1-gnu
-$EXEC_DIR/lmp_serial < in_equilibrate.txt -var pair_coeff_args 'library_CoNiCrFeMn.meam Co Ni Cr Fe Mn parameters.meam Co Ni Cr Fe Mn'
+$EXEC_DIR/lmp_serial < in_equilibrate.txt -var pair_coeff_args ${MEAM_library_DIR}/library_CoNiCrFeMn.meam Co Ni Cr Fe Mn ${MEAM_library_DIR}/parameters.meam Co Ni Cr Fe Mn
