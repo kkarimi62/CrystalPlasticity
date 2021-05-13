11a22f7431c81c9e8e6c2ef25986a2ecc8b8fd74
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-13 15:32:31 +0200

    update

---
+++ testRuns/Preparation/generate.py
@@ -0,0 +1,52 @@
+import numpy as np
+# 
+def Generate( natom, ntypes, 
+			  (xlo,xhi), (ylo, yhi), (zlo, zhi),
+			  title = 'data.txt',
+			  **kwargs ): 
+
+	x, y, z  = np.random.rand(	natom, 3 ).T
+
+	x *= ( xhi - xlo )
+	x += xlo
+	y *= ( yhi - ylo )
+	y += ylo
+	z *= ( zhi - zlo )
+	z += zlo
+	
+	#--- assign types
+	assert len(kwargs) == ntypes, 'provide number ration for each species!'
+	types = np.zeros((natom,), dtype=int)
+	atomi = 0
+	for key in kwargs:
+		ratio = kwargs[ key ]
+		itype = key[-1]
+
+		atomj = atomi + int( ratio * natom )
+		types[atomi:atomj] = itype
+
+		atomi = atomj
+	assert atomj <= natom
+	types[atomj:natom] = itype
+	#--- write output
+	sfile = open( title, 'w' )
+	print >> sfile, '# LAMMPS data file'
+	print >> sfile, '%s atoms'%natom
+	print >> sfile, '%s atom types'%ntypes
+	print >> sfile, '%s %s xlo xhi'%(xlo,xhi)
+	print >> sfile, '%s %s ylo yhi'%(ylo,yhi)
+	print >> sfile, '%s %s zlo zhi'%(zlo,zhi)
+	print >> sfile
+	print >> sfile, 'Atoms  # atomic: id tyoe x y z'
+	print >> sfile
+	for iatom in xrange( natom ): 
+		print >> sfile, '%s %s %s %s %s'%( iatom+1, types[ iatom ], x[ iatom ], y[ iatom ], z[ iatom ])
+	sfile.close()
+	
+if __name__ == '__main__':
+
+	Generate( 1000, 5, 
+			 (0,1.0), (0, 1.0), (0.0, 1.0),
+			 title = 'data.txt',
+			 ratio1 = 0.05, ratio2 = 0.26, ratio3 = 0.02, ratio4 = 0.4, ratio5 = 0.27 )	
+	
