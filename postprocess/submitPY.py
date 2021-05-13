ee02eb5191e8bed2d63bea68d3c90f7df4799d2a
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-13 11:51:40 +0200

    update

--- postprocess/submitPY.py
+++ postprocess/submitPY.py
@@ -27,7 +27,7 @@ if __name__ == '__main__':
 		string[ inums ] = "\tjobname  = '%s'\n" % ('%s%s'%(jobname,iphi)) #--- change job name
 		#---	densities
 		inums = lnums[ 1 ] - 1
-		string[ inums ] = "\targv=\'-p\tmccc\t%e\t-p\tbvall\t%e\t-p\tDfff\t%e\'\n"%(mc, bval, df)
+#		string[ inums ] = "\targv=\'-p\tmccc\t%e\t-p\tbvall\t%e\t-p\tDfff\t%e\'\n"%(mc, bval, df)
 
 		sfile=open('junk%s.py'%iphi,'w');sfile.writelines(string);sfile.close()
 		os.system( 'python junk%s.py'%iphi )
