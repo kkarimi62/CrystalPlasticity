d378d7ec3621a9d4e274a986a0183a09abf7924f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-13 21:02:19 +0200

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -17,7 +17,7 @@ def makeOAR( EXEC_DIR, node, core, time, PYFIL ):
 	OUT_PATH = '.'
 	if SCRATCH:
 		OUT_PATH = '/scratch/${SLURM_JOB_ID}'
-	print >> someFile, "$EXEC_DIR/%s < in.txt -var OUT_PATH %s pair_coeff_args $MEAM_library_DIR/library_CoNiCrFeMn.meam Co Ni Cr Fe Mn $MEAM_library_DIR/parameters.meam Co Ni Cr Fe Mn" %( PYFIL, OUT_PATH )
+	print >> someFile, "$EXEC_DIR/lmp_serial < in.txt -var OUT_PATH %s pair_coeff_args $MEAM_library_DIR/library_CoNiCrFeMn.meam Co Ni Cr Fe Mn $MEAM_library_DIR/parameters.meam Co Ni Cr Fe Mn" %( PYFIL, OUT_PATH )
 	someFile.close()										  
 
 
