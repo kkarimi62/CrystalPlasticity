bcf86aa58514ce97ac6b56a8796db4257171d280
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-14 21:13:40 +0200

    update

--- testRuns/Preparation/Preparation2nd.py
+++ testRuns/Preparation/Preparation2nd.py
@@ -3,8 +3,9 @@ def makeOAR( EXEC_DIR, node, core, time, PYFIL ):
 	print >> someFile, '#!/bin/bash\n'
 	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
 	print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
-	print >> someFile, 'module load mpich/3.2.1-gnu\n'
+	print >> someFile, 'module load mpich/3.2.1-gnu'
 
+	#--- run python script 
 	#--- run python script
 	pyScript = open( '%s/pyScript.py'%writPath, 'w' )
 	print >> pyScript, 'import imp\ngn=imp.load_source(\'generate.name\',\'%s/generate.py\')'%(PYFIL)
@@ -12,13 +13,18 @@ def makeOAR( EXEC_DIR, node, core, time, PYFIL ):
 	pyScript.close()
 	print >> someFile, 'python pyScript.py\n'
 
-	#--- run python script 
+	#--- parse sample height
+#	print >> someFile, "IFS=\',\' read -ra a <<< \"$string\"" #--- split into array
+#	print >> someFile, "zmax=${a[0]} #--- height"
+#	print >> someFile, "rmax=${a[1]} #--- max bond length"
+#	print >> someFile, "echo ${zmax}"
+#	print >> someFile, "echo${rmax}"
+
+	#--- run lammps: compression test
 	OUT_PATH = '.'
 	if SCRATCH:
 		OUT_PATH = '/scratch/${SLURM_JOB_ID}'
-#	print >> someFile, "$EXEC_DIR/%s < in.txt -var OUT_PATH %s -var MEAM_library_DIR %s"%( EXEC, OUT_PATH, MEAM_library_DIR )
-	cutoff = 1.5 / rho ** (1.0/3.0)
-	print >> someFile, "$EXEC_DIR/%s < in.txt -echo screen -var OUT_PATH %s -var MEAM_library_DIR %s -var cutoff %s"%( EXEC, OUT_PATH, MEAM_library_DIR, cutoff )
+	print >> someFile, "mpirun -np %s $EXEC_DIR/%s < in.txt -var OUT_PATH %s -var MEAM_library_DIR %s -echo screen" %( nThreads, EXEC, OUT_PATH, MEAM_library_DIR )
 	someFile.close()										  
 
 
@@ -27,22 +33,16 @@ if __name__ == '__main__':
 
 	nruns	 = 1
 	nThreads = 1
-	jobname  = 'test'
-#	sourcePath = os.getcwd() + '/dataFiles'
+	jobname  = 'test2nd'
+	sourcePath = os.getcwd() + '/test' #--- must be different than sourcePath
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/CrystalPlasticity/lammps-29Oct20/src' #--- path for executable file
 	MEAM_library_DIR='/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles' #--- meam potential parameters
 	PYFIL = '/home/kamran.karimi1/Project/git/CrystalPlasticity/py'
-	EXEC = 'lmp_serial'
-	durtn = '00:29:59'
+	EXEC = 'lmp_mpi' 
+	durtn = '00:59:59'
 	SCRATCH = None
 	partition = 'single' #'parallel'
-	#--- sim. parameters
-#	(xlo, xhi ) = (0.0, 1.0)
-#	(ylo, yhi ) = (0.0, 1.0)
-#	(zlo, zhi ) = (0.0, 1.0)
-	natom = 10000 #50688 
-	ntypes = 5
-	rho = 0.1
+	#--- update data.txt and lammps script
 	#---
 	os.system( 'rm -rf %s' % jobname ) #--- rm existing
 	os.system( 'rm jobID.txt' )
@@ -54,9 +54,10 @@ if __name__ == '__main__':
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
 		if irun == 0: #--- cp to directory
 			path=os.getcwd() + '/%s' % ( jobname)
-			os.system( 'cp %s/%s %s' % ( EXEC_DIR, EXEC, path ) ) # --- create folder & mv oar scrip & cp executable
+			os.system( 'cp %s/%s %s' % ( EXEC_DIR, PYFIL, path ) ) # --- create folder & mv oar scrip & cp executable
 		#---
-		os.system( 'cp in_soft.txt %s/in.txt ' % writPath ) #--- lammps script: periodic x, pxx, vy, load
+		os.system( 'cp in_equilibrate.txt %s/in.txt ' % writPath ) #--- lammps script: periodic x, pxx, vy, load
+#		os.system( 'cp %s/Run%s/xyz_cords.dump %s/Run%s/dump.neighbors %s ' % (sourcePath, irun, sourcePath, irun, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		#---
 		#---
 		makeOAR( path, 1, nThreads, durtn, PYFIL ) # --- make oar script
