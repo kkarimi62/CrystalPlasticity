1b974cf537050ffcef387e43ac2518c71df75371
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-05-15 19:40:34 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -1162,10 +1162,10 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
             if (!iszero(dsij1) || !iszero(dsij2) || !iszero(ddsddrik) || !iszero(ddsddrjk) ) {
               force1 = dUdsij * dsij1;
               force2 = dUdsij * dsij2;
-            //--- add stiffness
-              stif1 =  ddUddsij * dsij1 * dsij1 * rik2 + ddUdrijds * 2.0 * dsij1 * rik + dUdsij * ( - dsij1 + ddsddrik  ); //--- units of u/r^2 
+            //--- add stiffness uncomment!!!!!!!!
+              stif1 =  0.0;//ddUddsij * dsij1 * dsij1 * rik2 + ddUdrijds * 2.0 * dsij1 * rik + dUdsij * ( - dsij1 + ddsddrik  ); //--- units of u/r^2 
               stif1 *= rik2; //--- units of energy
-              stif2 =  ddUddsij * dsij2 * dsij2 * rjk2 + ddUdrijds * 2.0 * dsij2 * rjk + dUdsij * ( - dsij2 + ddsddrjk  );
+              stif2 =  0.0;//ddUddsij * dsij2 * dsij2 * rjk2 + ddUdrijds * 2.0 * dsij2 * rjk + dUdsij * ( - dsij2 + ddsddrjk  );
               stif2 *= rjk2;
               //
               f[i][0] += force1 * dxik;
