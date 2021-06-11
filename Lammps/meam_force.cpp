8edb7f4b2b9a185d4839d950bc5d1f75ec17b3fa
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-12 00:36:25 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -997,7 +997,8 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         }
 
         //--- add stiffness (units of u/r^2)
-        stiff = ddUddrij - dUdrij * recip; 
+//kam        stiff = ddUddrij - dUdrij * recip; 
+        stiff = ddUddrij + dUdrij * recip; 
         stiff0 = 0.0; 
         stiff1 = 0.0;
         stiff2 = 0.0;
