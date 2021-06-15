00a6307aada9b978e4ceb26ee464e0d13b16c9fb
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-15 03:05:07 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -1056,7 +1056,7 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
        
 
           double r3 = rij*rij*rij;
-          double ds = dscrfcn[fnoffset + jn];// * rij; ???????
+          double ds = dscrfcn[fnoffset + jn] * rij; ???????
           double dds = ddscrfcn[fnoffset + jn];
                       
 
