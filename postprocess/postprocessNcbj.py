a71151f74c72394c20e672bf565ab664074e5b78
Kamran Karimi <kkarimi@usrint2.cis.gov.pl>
2021-06-21 11:06:35 +0200

    update

---
+++ postprocess/postprocessNcbj.py
@@ -0,0 +1,43 @@
+def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
+	someFile = open( 'oarScript.sh', 'w' )
+	print >> someFile, '#!/bin/bash\n'
+	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
+	print >> someFile, 'papermill --prepare-only %s/%s ./output.ipynb %s %s'%(EXEC_DIR,PYFIL,argv,argv2nd) #--- write notebook with a list of passed params
+	print >> someFile, 'jupyter nbconvert --execute %s/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(EXEC_DIR)
+	someFile.close()										  
+#
+if __name__ == '__main__':
+	import os
+#
+	nruns	 = 1
+	jobname  = 'test' 
+	readPath = os.getcwd() + '/../testRuns/glassFeNi' # --- source
+	EXEC_DIR = '.'     #--- path for executable file
+	durtn = '00:59:59'
+	resources = {'mem':'4gb', 'partition':'a12h','nodes':1,'ppn':1}
+	argv = " -p path \'%s"%(readPath) 
+	argv2nd = "" #lambdc 1.0e5"
+	PYFILdic = { 
+		0:'ElasticConstants.ipynb',
+		1:'analyzePlasticity.ipynb',
+		}
+	keyno = 1
+#---
+#---
+	PYFIL = PYFILdic[ keyno ] 
+	#--- update argV
+	#---
+	os.system( 'rm -rf %s' % jobname ) # --- rm existing
+	# --- loop for submitting multiple jobs
+	counter = 0
+	for irun in xrange( nruns ):
+		print ' i = %s' % counter
+		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
+		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
+		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s\'"%irun, argv2nd) # --- make oar script
+		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
+		os.system( 'qsub -q %s -l nodes=%s:ppn=%s -N %s.%s -o %s -e %s -d %s  %s/oarScript.sh'\
+			%( resources['partition'], resources['nodes'], resources['ppn'], jobname, counter, writPath, writPath, writPath , writPath ) ) # --- runs oarScript.sh!
+		counter += 1
+											 
+
