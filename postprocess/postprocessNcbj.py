204795a2bd7f464f1882dd788bc35cd255b9672c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-26 00:15:41 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
 	os.system( 'rm -rf %s' % jobname ) # --- rm existing
 	# --- loop for submitting multiple jobs
 	counter = 0
-	for irun in xrange( nruns ):
+	for irun in range( nruns ):
 		print(' i = %s' % counter)
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
