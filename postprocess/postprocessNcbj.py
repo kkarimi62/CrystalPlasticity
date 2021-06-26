938f6f6d3210330e0357a1b0aae148b8fbc7c891
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-26 18:59:56 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -4,7 +4,8 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
 	print('EXEC_DIR=%s\n' %( EXEC_DIR ), file=someFile)
 #	print('JUPYT_DIR=/mnt/opt/tools/slc6/python-jupyter/dev-x86_64-gcc46-python35/bin\n',file=someFile)
 	print('source activate test-env;papermill --prepare-only $EXEC_DIR/%s ./output.ipynb %s %s\n'%(PYFIL,argv,argv2nd), file=someFile)
-	print('conda deactivate; jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
+#	print('conda deactivate; jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
+	print('jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
 	someFile.close()										  
 #
 if __name__ == '__main__':
