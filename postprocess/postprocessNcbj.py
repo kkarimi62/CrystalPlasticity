374f4fb1fb647ba7b7a807ec04927419ccf04800
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-26 20:35:59 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -3,9 +3,10 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
 	print('#!/bin/bash\n', file=someFile)
 	print('EXEC_DIR=%s\n' %( EXEC_DIR ), file=someFile)
 #	print('JUPYT_DIR=/mnt/opt/tools/slc6/python-jupyter/dev-x86_64-gcc46-python35/bin\n',file=someFile)
-	print('source activate test2nd-env;papermill --prepare-only $EXEC_DIR/%s ./output.ipynb %s %s\n'%(PYFIL,argv,argv2nd), file=someFile)
+#	print('source activate test2nd-env;papermill --prepare-only $EXEC_DIR/%s ./output.ipynb %s %s\n'%(PYFIL,argv,argv2nd), file=someFile)
 #	print('conda deactivate; jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
-	print('jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
+#	print('jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
+	print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
 	someFile.close()										  
 #
 if __name__ == '__main__':
