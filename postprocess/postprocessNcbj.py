59975e55ed68b42f04e08b524d0e2f22f5de182b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-26 21:41:17 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -1,7 +1,13 @@
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
+	#--- set environment variables
+	sfile = open('.env','w')
+	print('%s\n%s'(argv,argv2nd),file=sfile)
+	sfile.close()
+	#---
 	someFile = open( 'oarScript.sh', 'w' )
 	print('#!/bin/bash\n', file=someFile)
 	print('EXEC_DIR=%s\n' %( EXEC_DIR ), file=someFile)
+	
 #	print('JUPYT_DIR=/mnt/opt/tools/slc6/python-jupyter/dev-x86_64-gcc46-python35/bin\n',file=someFile)
 #	print('source activate test2nd-env;papermill --prepare-only $EXEC_DIR/%s ./output.ipynb %s %s\n'%(PYFIL,argv,argv2nd), file=someFile)
 #	print('conda deactivate; jupyter nbconvert --execute $EXEC_DIR/output.ipynb --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html', file=someFile)
@@ -18,7 +24,8 @@ if __name__ == '__main__':
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
 	resources = {'mem':'4gb', 'partition':'i12h','nodes':1,'ppn':1}
-	argv = " -p path \'%s"%(readPath) 
+#	argv = " -p path \'%s"%(readPath) 
+	argv = "path=\'%s"%(readPath) 
 	argv2nd = "" # -p itime %s"%(2000000) 
 	PYFILdic = { 
 		0:'ElasticConstants.ipynb',
@@ -40,7 +47,7 @@ if __name__ == '__main__':
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
 		os.system( 'cp LammpsPostProcess.py %s' % ( writPath ) ) #--- cp python module
 		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s\'"%irun, argv2nd) # --- make oar script
-		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
+		os.system( 'chmod +x oarScript.sh; mv oarScript.sh .env %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
 		os.system( 'qsub -q %s -l nodes=%s:ppn=%s -N %s.%s -o %s -e %s -d %s  %s/oarScript.sh'\
 			%( resources['partition'], resources['nodes'], resources['ppn'], jobname, counter, writPath, writPath, writPath , writPath ) ) # --- runs oarScript.sh!
 		counter += 1
