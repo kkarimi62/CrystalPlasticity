f023439477f755e3b169a9f233df23b987663d97
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-27 19:51:21 +0200

    Update meam.h

--- Lammps/meam.h
+++ Lammps/meam.h
@@ -365,7 +365,7 @@ protected:
                         double* drhodrm,
                         double* ddrhodrmds //--- modify
                        );
-  double GetModulus(    int i, int j, double** x, int numneigh, int* firstneigh, int numneigh_full, int* firstneigh_full, int* type, int* fmap,
+  double GetModulus(    int i, int j, double** x, int numneigh, int* firstneigh, int numneigh_full, int* firstneigh_full, int* type, int* fmap, double sij,
                         int alpha, int beta, int gamma, int lambda,  double r3,double ds, double dds, double recip,
                         double dUdrij, double dUdsij, double ddUddrij, double ddUdrijds, double ddUddsij,
                         double* dUdrijm, double* delij, double* ddUdrdrijm, double* ddUdrijmds, double* ddUdrmdrn);
