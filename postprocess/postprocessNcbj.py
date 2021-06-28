88ebafc6c49c2d751cd9cbf99ad69dbfd1837f0a
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-28 14:01:55 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -14,11 +14,11 @@ if __name__ == '__main__':
 	import os
 
 	nruns	 = 3
-	jobname  = 'd2minTest5th' 
+	jobname  = 'd2minTest6th' 
 	readPath = os.getcwd() + '/../testRuns/glassFeNi' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
-	resources = {'mem':'4gb', 'partition':'i12h','nodes':1,'ppn':1}
+	resources = {'mem':'8gb', 'partition':'i12h','nodes':1,'ppn':1}
 	argv = "path=%s"%(readPath) #--- don't change! 
 	argv2nd = "junk=None" 
 	PYFILdic = { 
