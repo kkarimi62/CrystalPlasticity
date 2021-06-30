989be9c7eb5b8a7c0a6b807ec6bb5de77cfca430
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-06-30 19:14:26 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 	import os
 
 	nruns	 = range(3)
-	jobname  = 'yieldDStrsCoNiFe' 
-	readPath = os.getcwd() + '/../testRuns/glassCoNiFe' # --- source
+	jobname  = 'yieldDStrsCoNiCrFe' 
+	readPath = os.getcwd() + '/../testRuns/glassCoNiCrFe' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
 	resources = {'mem':'4gb', 'partition':'o12h','nodes':1,'ppn':1}
