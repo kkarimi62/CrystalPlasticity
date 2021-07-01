f4f48da8b0a08bb2126acb5d699ac955321d97f4
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-01 20:56:36 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -1,15 +1,20 @@
 def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
+	#--- set environment variables
+	sfile = open('.env','w')
+	print('%s\n%s'%(argv,argv2nd),file=sfile)
+	sfile.close()
+
 	someFile = open( 'oarScript.sh', 'w' )
-	print >> someFile, '#!/bin/bash\n'
-	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
-	print >> someFile, 'papermill --prepare-only %s/%s ./output.ipynb %s %s'%(EXEC_DIR,PYFIL,argv,argv2nd) #--- write notebook with a list of passed params
-	print >> someFile, 'jupyter nbconvert --execute %s/output.ipynb --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(EXEC_DIR)
+	print('#!/bin/bash\n',file=someFile)
+	print('EXEC_DIR=%s\n'%( EXEC_DIR ),file=someFile)
+#	print >> someFile, 'papermill --prepare-only %s/%s ./output.ipynb %s %s'%(EXEC_DIR,PYFIL,argv,argv2nd) #--- write notebook with a list of passed params
+	print >> someFile, 'jupyter nbconvert --execute $EXEC_DIR/%s --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL)
 	someFile.close()										  
 #
 if __name__ == '__main__':
 	import os
 #
-	nruns	 = 1
+	nruns	 = [1] #range(3)
 	jobname  = 'glassFeNi' 
 	readPath = os.getcwd() + '/../testRuns/glassFeNi' # --- source
 #	readPath = os.getcwd() + '/../BmgData' # --- source
@@ -17,8 +22,10 @@ if __name__ == '__main__':
 	durtn = '00:59:59'
 	mem = '8gb'
 	partition = 'single' #'cpu2019' #'bigmem' #'single' #'parallel' #'single'
-	argv = " -p path \'%s"%(readPath) #--- don't change! 
-	argv2nd = " -p itime %s"%(0) 
+#	argv = " -p path \'%s"%(readPath) #--- don't change! 
+#	argv2nd = " -p itime %s"%(0) 
+	argv = "path=%s"%(readPath) #--- don't change! 
+	argv2nd = "junk=junk"#"itime=10000" 
 	PYFILdic = { 
 		0:'ElasticConstants.ipynb',
 		1:'analyzePlasticity.ipynb',
@@ -32,17 +39,16 @@ if __name__ == '__main__':
 	os.system( 'rm -rf %s' % jobname ) # --- rm existing
 	# --- loop for submitting multiple jobs
 	counter = 0
-	for irun in xrange( nruns ):
+	for counter in nruns:
 		print ' i = %s' % counter
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
 		os.system( 'cp LammpsPostProcess.py %s' % ( writPath ) ) #--- cp python module
-		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s\'"%irun, argv2nd) # --- make oar script
+		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s\'"%counter, argv2nd) # --- make oar script
 		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
 		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
 						    --chdir %s -c %s -n %s %s/oarScript.sh'\
 						   % ( partition, mem, durtn, jobname, counter, jobname, counter, jobname, counter \
 						       , writPath, 1, 1, writPath ) ) # --- runs oarScript.sh!
-		counter += 1
 											 
 
