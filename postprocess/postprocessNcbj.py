5b3868c6ad74f50265b6788daf8bbb559f4bc0fc
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-02 16:19:26 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 	import os
 
 	nruns	 = range(3)
-	jobname  = 'yieldVsWidthFeNi' 
+	jobname  = 'yieldVsWidthFeNi2nd' 
 	readPath = os.getcwd() + '/../testRuns/glassFeNi' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
