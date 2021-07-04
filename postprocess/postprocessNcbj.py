77bd420209aa05c8394e4f22eb0e2d9dd00f1359
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-04 16:08:05 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
 	import os
 
 	nruns	 = range(3)
-	jobname  = 'StrsDropCo5Cr5Fe40Mn27Ni26' 
-	readPath = os.getcwd() + '/../testRuns/glassCo5Cr5Fe40Mn27Ni26' # --- source
+	jobname  = 'StrsDropCoCrFeMn' 
+	readPath = os.getcwd() + '/../testRuns/glassCoCrFeMn' # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
 	resources = {'mem':'16gb', 'partition':'o12h','nodes':1,'ppn':1}
