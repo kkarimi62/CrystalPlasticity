fe38dbc76e4264519eb1ce6e6681dc3b0e6551a2
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-06 22:29:31 +0200

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
 	EXEC = 'lmp_mpi' #'lmp_serial'
 	durtn = '95:59:59'
 	SCRATCH = True
-	mem = '8gb'
+	mem = '4gb'
 	partition = 'parallel' #'single' #'parallel'
 	#--- sim. parameters
 	natom = 50000 
