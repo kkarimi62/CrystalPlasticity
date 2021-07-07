8dac851f5cab50c280ad8d0818f0f4a7d997aaec
kamran.karimi1 <kkarimi62>
2021-07-07 01:59:42 -0600

    update

--- testRuns/Preparation/cpu.py
+++ testRuns/Preparation/cpu.py
@@ -1,9 +1,16 @@
-
+import pdb
 import os
 
 PHI=[1,2,4,6,8,10,12]
 n=len(PHI)
 for i in xrange(n):
 	path = 'testCpuRunTime%s/Run0/log.lammps'%(i)
-	os.system('grep %s %s'%('Loop time of',path))
+	os.system('grep %s %s>>junk.txt'%("\'Loop time of\'",path))
 
+
+string = open('junk.txt').readlines()
+try:
+    for i in xrange(n):
+        print PHI[i],string[i][13:string[i].find("on")-1]
+except:
+    pass
