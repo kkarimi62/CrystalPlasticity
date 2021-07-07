43f5355db405292fb580bc1cd61ce47245c76228
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-07 14:15:27 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -952,7 +952,7 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         //     Compute derivatives of energy wrt rij, sij, and rij[3]
 //          sij = rij*rij;
           double r3 = rij*rij*rij;
-          double ds = dscrfcn[fnoffset + jn];// * rij;
+          double ds = dscrfcn[fnoffset + jn] * rij;
 //        double ds = 2*rij;dscrfcn[fnoffset + jn]=ds/rij;
           double dds = ddscrfcn[fnoffset + jn];
 //        double dds = 2.0;
