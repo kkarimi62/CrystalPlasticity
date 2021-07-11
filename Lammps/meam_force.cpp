7c98d87e2de17d6236194986c3b0bb592ed00112
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-11 14:23:03 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -985,10 +985,10 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         ddUdrijds = 0.0;
         if (!iszero(dscrfcn[fnoffset + jn])) {
           dUdsij = phi + frhop[i] * drhods1;// + frhop[j] * drhods2; //--- Eq. 4.41(b)
-           ddUddsij = frhopp[i] * drhods1 * drhods1 + frhop[i] * ddrhodsds1;// +
-     //                 frhopp[j] * drhods2 * drhods2 + frhop[j] * ddrhodsds2;
+           ddUddsij = frhopp[i] * drhods1 * drhods1 + frhop[i] * ddrhodsds1 +
+                      frhopp[j] * drhods2 * drhods2 + frhop[j] * ddrhodsds2;
 //        if((i==0 and j==1) or (i==1 and j==0) )
-             fprintf(pFile, "%d %d %e %e %e\n",i,j,rho[i],frhop[i],frhopp[i]);
+//             fprintf(pFile, "%d %d %e %e %e\n",i,j,rho[i],frhop[i],frhopp[i]);
           
           
           for (m = 0; m < 3; m++) ddUdrijmds[m] = frhopp[i] * drhods1 * drhodrm1[m] + frhop[i] * ddrhodrmds1[m] +
