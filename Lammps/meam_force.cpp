06973b790842bf8e0efa80ed90935c5d1edbe3e9
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-12 12:40:43 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -982,7 +982,7 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
         
         
         
-//         frhop[i] = 1.0;frhopp[i]=0.0;
+         frhop[i] = 1.0;frhopp[i]=0.0;
          frhop[j] = 0.0;frhopp[j]=0.0;
         phi=0.0;phip=0.0;phipp=0.0;
         dUdrij = phip * sij + frhop[i] * drho1dr1 + frhop[j] * drho1dr2; //--- Eq. 4.41(a)
