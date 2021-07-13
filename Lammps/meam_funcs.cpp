f629797c228dbdb5e2fd5bba16e96a45ddab9891
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-13 12:29:52 +0200

    Update meam_funcs.cpp

--- Lammps/meam_funcs.cpp
+++ Lammps/meam_funcs.cpp
@@ -1012,6 +1012,7 @@ double MEAM::GetModulus(int i, int j, double** x, int numneigh, int* firstneigh,
                       sikj = dfcut(cikj, dfc, ddfc);
                       dsikj = dfc * dcikj;
                       dCfunc2(rij2, rik2, rjk2, dCikj1, dCikj2); //--- 4.17b/rik, 4.17c/rjk
+                      ddCfunc2(rij2, rik2, rjk2, ddCikj1, ddCikj2 );
                       dsij=ds;
                       assert(!iszero(sikj));
                       a = sij / delc * dfc / sikj;
