2d5df191d4c9078b3c94d965b1cb55e7bd4d8c9b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-21 10:48:27 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -310,8 +310,8 @@ MEAM::meam_force(int i, int eflag_either, int eflag_global, int eflag_atom, int 
 	 darho3bdsj[m] = 0.0;//rhoa3i * delij[m] / rij;
          for (n = m; n < 3; n++) {
             for (p = n; p < 3; p++) {
-               darho3dri[nv3] = A3j_d * delij[m] * delij[n] * delij[p] * sij; //--- deriv. Eq. 4.27(c) wrt rij 
-               darho3drj[nv3] = A3i_d * delij[m] * delij[n] * delij[p] * sij;
+               darho3dri[nv3] = A3j_d * delij[m] * delij[n] * delij[p];//A3j_d * delij[m] * delij[n] * delij[p] * sij; //--- deriv. Eq. 4.27(c) wrt rij 
+               darho3drj[nv3] = A3i_d * delij[m] * delij[n] * delij[p];//A3i_d * delij[m] * delij[n] * delij[p] * sij;
 	       darho3dsi[nv3] = 0.0;//A3j * delij[m] * delij[n] * delij[p];
 	       darho3dsj[nv3] = 0.0;//A3i * delij[m] * delij[n] * delij[p];
 
