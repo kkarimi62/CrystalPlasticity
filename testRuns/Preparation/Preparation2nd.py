26852ce1c92b6d1687f01e4a33d7649ecd530c03
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-27 17:08:19 +0200

    update

--- testRuns/Preparation/Preparation2nd.py
+++ testRuns/Preparation/Preparation2nd.py
@@ -36,8 +36,8 @@ if __name__ == '__main__':
 
 	nruns	 = 1
 	nThreads = 32
-	jobname  = 'Natom250kQrate0.5Tstop2333Srate-5'
-	sourcePath = os.getcwd() + '/Natom250kQrate0.5Tstop2333' #--- must be different than sourcePath
+	jobname  = 'Natom250kQrate0.5Tstop2200Srate-5'
+	sourcePath = os.getcwd() + '/Natom250kQrate0.5Tstop2200' #--- must be different than sourcePath
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
 	MEAM_library_DIR='/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles' #--- meam potential parameters
 	PYFIL = '/home/kamran.karimi1/Project/git/CrystalPlasticity/py'
