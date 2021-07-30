4f04f1411c5788082283c20378c17241ab0c16af
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-30 21:21:45 +0200

    update

--- testRuns/submitPY.py
+++ testRuns/submitPY.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
 	import numpy as np
 	#---
 	lnums = [ 16, 17 ]
-	string=open('testRuns2ndNcbj.py').readlines() #--- python script
+	string=open('testRuns2nd.py').readlines() #--- python script
 	#---
 	PHI = [	 'FeNi', 
              'CoNiFe', 
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 		inums = lnums[ 1 ] - 1
 #		string[ inums ] = "\targv=\'-p\tmccc\t%e\t-p\tbvall\t%e\t-p\tDfff\t%e\'\n"%(mc, bval, df)
 #		string[ inums ] = "\targv2nd=\' -p\titime\t%s\'\n"%(PHI[iphi])
-		string[ inums ] = "\targs=\'%s_glass.dump %s_Cna.xyz\'\n"%(PHI[iphi],PHI[iphi])
+		string[ inums ] = "\targs=\'%s_glass.dump %s_gr.xyz 1\'\n"%(PHI[iphi],PHI[iphi])
 
 		sfile=open('junk%s.py'%iphi,'w');sfile.writelines(string);sfile.close()
 		os.system( 'python3 junk%s.py'%iphi )
