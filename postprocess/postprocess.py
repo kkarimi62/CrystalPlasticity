5e6a75c2dfe92f18b6dbd76eb14b4e8c8951e581
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-31 16:14:55 +0200

    updates

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = range(1)
-	jobname  = 'test11thMelt2ndNatom50kQrate1Srate6Strs' 
+	jobname  = 'Natom250kQrate0.5Tstop1800Srate-5Strs' 
 	DeleteExistingFolder = True
-	readPath = os.getcwd() + '/../testRuns/Preparation/test11thMelt2ndNatom50kQrate1Srate6' #--- source
+	readPath = os.getcwd() + '/../testRuns/Preparation/Natom250kQrate0.5Tstop1800Srate-5' #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
 	mem = '16gb'
