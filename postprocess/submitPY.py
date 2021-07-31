fc8d2538c146a6ccd0c7d49c1b838893e4d5efd3
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-07-31 16:27:06 +0200

    updates

--- postprocess/submitPY.py
+++ postprocess/submitPY.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-	lnums = [ 18, 26 ]
+	lnums = [ 18, 20 ]
 	string=open('postprocess.py').readlines() #--- python script
 	#---
 	'''
@@ -14,26 +14,27 @@ if __name__ == '__main__':
              'CoNiCrFeMn',
              'Co5Cr5Fe40Mn27Ni26']
 	'''
-	PHI=range(0,2000000+10000,4*100000) 
+	PHI = [1800,1933,2066,2200,2333]
+#	PHI=range(0,2000000+10000,4*100000) 
 	nphi = len(PHI)
 	#---
 #	nn = 4
 #	NTHRESH = np.linspace(0.05,0.11,nn,endpoint=True)
 	#---
-	jobname = 'CnaFeNiItime'
+	jobname = ''
 
 #	PHI = [[PHI[iphi],NTHRESH[inn]] for iphi in xrange( nphi ) for inn in xrange(nn)]
 #	nphi = len(PHI)
 	for iphi in range( nphi ):
 		#---	
 		inums = lnums[ 0 ] - 1
-		string[ inums ] = "\tjobname  = '%s'\n" % ('%s%s'%(jobname,PHI[iphi])) #--- change job name
+		string[ inums ] = "\t\'Natom250kQrate0.5Tstop%sSrate-5Strs'\n" % (PHI[iphi]) #--- change job name
 		#---	densities
 		inums = lnums[ 1 ] - 1
-#		string[ inums ] = "\treadPath = os.getcwd() + \'/../testRuns/glass%s\'\n"%(PHI[iphi])
+		string[ inums ] = "\treadPath = os.getcwd() + \'/../testRuns/Preparation/Natom250kQrate0.5Tstop%sSrate-5\'\n"%(PHI[iphi])
 #
 #		inums = lnums[ 2 ] - 1
-		string[ inums ] = "\targv2nd = \'itime=%s\'\n"%(PHI[iphi])
+#		string[ inums ] = "\targv2nd = \'itime=%s\'\n"%(PHI[iphi])
 
 		sfile=open('junk%s.py'%iphi,'w');sfile.writelines(string);sfile.close()
 		os.system( 'python3 junk%s.py'%iphi )
