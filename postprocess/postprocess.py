c639c7a8a97e82eb3d0ddadc214bc934135d88ee
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-08-12 16:55:05 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -19,9 +19,9 @@ if __name__ == '__main__':
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + '/../testRuns/glassCo5Cr2Fe40Mn27Ni26' #--- source
 	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '03:59:59'
+	durtn = '00:59:59'
 	mem = '16gb'
-	partition = 'parallel' #'cpu2019' #'bigmem' #'single' #'parallel' #'single'
+	partition = 'bigmem' #'cpu2019' #'bigmem' #'single' #'parallel' #'single'
 	argv = "path=%s"%(readPath) #--- don't change! 
 	argv2nd = "indx=5" 
 	PYFILdic = { 
