9e2428eb52772c58fb39831bf09e746c62069468
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-08-12 16:56:41 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -20,8 +20,8 @@ if __name__ == '__main__':
 	readPath = os.getcwd() + '/../testRuns/glassCo5Cr2Fe40Mn27Ni26' #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
-	mem = '16gb'
-	partition = 'bigmem' #'cpu2019' #'bigmem' #'single' #'parallel' #'single'
+	mem = '24gb'
+	partition = 'parallel' #'cpu2019' #'bigmem' #'single' #'parallel' #'single'
 	argv = "path=%s"%(readPath) #--- don't change! 
 	argv2nd = "indx=5" 
 	PYFILdic = { 
