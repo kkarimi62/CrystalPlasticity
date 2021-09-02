8ccea8dfb6ed515eebf5aed995f1bc2d307cb79c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-09-02 10:55:53 +0200

    Update meam_force.cpp

--- Lammps/meam_force.cpp
+++ Lammps/meam_force.cpp
@@ -1086,8 +1086,8 @@ double  ak, ro0k, rhoa2k;
         }
 
 	if(i==0 and j == 1)
-		fprintf ( pFile, "%e %e %e %e %e %e %e %e %e %e %e %e\n", sij, rho2[i], drho2dr1, drho2ds1, drho2drm1[0], drho2drm1[1], drho2drm1[2], 
-		ddrho2drdr1, ddrho2drds1, ddrho2drmds1[0], ddrho2drmds1[1], ddrho2drmds1[2] );
+		fprintf ( pFile, "%e %e %e %e %e %e %e %e %e %e %e %e\n", sij, rho1[i], drho1dr1, drho1ds1, drho1drm1[0], drho1drm1[1], drho1drm1[2], 
+		ddrho1drdr1, ddrho1drds1, ddrho1drmds1[0], ddrho1drmds1[1], ddrho1drmds1[2] );
 
         //     Compute derivatives of energy wrt rij, sij, and rij[3]
 //          sij = rij*rij;
