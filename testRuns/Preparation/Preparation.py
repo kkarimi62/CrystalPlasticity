4eae5578c92920977aabc9d088524277972f86d1
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-11-01 11:53:56 +0100

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -1,59 +1,100 @@
-def makeOAR( EXEC_DIR, node, core, time, PYFIL ):
+def makeOAR( EXEC_DIR, node, core, time ):
 	someFile = open( 'oarScript.sh', 'w' )
 	print >> someFile, '#!/bin/bash\n'
 	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
 	print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
 	print >> someFile, 'module load mpich/3.2.1-gnu\n'
 
-        '''
-	#--- run python script
-	pyScript = open( '%s/pyScript.py'%writPath, 'w' )
-	print >> pyScript, 'import imp\ngn=imp.load_source(\'generate.name\',\'%s/generate.py\')'%(PYFIL)
-	print >> pyScript, 'gn.Generate( %s, %s, %s,title = \'data.txt\',ratio1 = %s, ratio2 = %s, ratio3 = %s, ratio4 = %s, ratio5 = %s )'%(natom, ntypes, rho, 0.05, 0.26, 0.02, 0.4, 0.27)
-	pyScript.close()
-	print >> someFile, 'python pyScript.py\n'
-        '''
-
 	#--- run python script 
-	OUT_PATH = '.'
-	if SCRATCH:
-		OUT_PATH = '/scratch/${SLURM_JOB_ID}'
 #	 print >> someFile, "$EXEC_DIR/%s < in.txt -var OUT_PATH %s -var MEAM_library_DIR %s"%( EXEC, OUT_PATH, MEAM_library_DIR )
 #	cutoff = 1.0 / rho ** (1.0/3.0)
-	if EXEC == 'lmp_serial': 
-		print >> someFile, "$EXEC_DIR/%s < in.txt -echo screen -var OUT_PATH %s -var MEAM_library_DIR %s -var cutoff %s -var natom %s -var tstop %s"%( EXEC, OUT_PATH, MEAM_library_DIR, cutoff, natom, Tfinal )
-	elif EXEC == 'lmp_mpi':
-		print >> someFile, "mpirun -np %s $EXEC_DIR/%s < in.txt -echo screen -var OUT_PATH %s -var MEAM_library_DIR %s -var cutoff %s -var natom %s -var tstop %s"%( nThreads*nNode, EXEC, OUT_PATH, MEAM_library_DIR, cutoff, natom, Tfinal )
-	elif EXEC == 'lmp_kokkos_cuda':
-		print >> someFile, "mpirun -np %s $EXEC_DIR/%s -k on g 1 t 1 -sf kk -pk kokkos binsize 2.8 comm device < in.txt -echo screen -var OUT_PATH %s -var MEAM_library_DIR %s -var cutoff %s -var natom %s -var tstop %s"%( nThreads*nNode, EXEC, OUT_PATH, MEAM_library_DIR, cutoff, natom, Tfinal )
-	elif EXEC == 'lmp_gpu':
-		print >> someFile, "mpirun -np %s $EXEC_DIR/%s -sf gpu -pk gpu 2 < in.txt -echo screen -var OUT_PATH %s -var MEAM_library_DIR %s -var cutoff %s -var natom %s -var tstop %s"%( nThreads*nNode, EXEC, OUT_PATH, MEAM_library_DIR, cutoff, natom, Tfinal )
+	for script,var,indx, execc in zip(Pipeline,Variables,range(100),EXEC):
+		if execc == 'lmp': #_mpi' or EXEC == 'lmp_serial':
+			print >> someFile, "mpirun -np %s $EXEC_DIR/lmp_mpi < %s -echo screen -var OUT_PATH %s -var PathEam %s -var INC %s %s"%(nThreads*nNode, script, OUT_PATH, MEAM_library_DIR, SCRPT_DIR, var)
+		elif execc == 'py':
+			print >> someFile, "python3 %s %s"%(script, var)
+			
 	someFile.close()										  
 
 
 if __name__ == '__main__':
 	import os
-        import numpy as np
+	import numpy as np
 
 	nruns	 = 1
-	nThreads = 12
-	nNode	 = 	1
-	jobname  = 'gpuBenchmark' #'Natom25kQrate0.5'
+	#
+	nThreads = [1,9][0]
+	nNode	 = 1
+	#
+	jobname  = {
+				1:'CuZrNatom1K',
+			   }[9]
+	sourcePath = os.getcwd() +\
+				{	
+					1:'/../postprocess/NiCoCrNatom1K',
+					4:'/junk',
+				}[4] #--- must be different than sourcePath
+        #
+	sourceFiles = { 0:False,
+					1:['Equilibrated_300.dat'],
+					2:['data.txt','ScriptGroup.txt'],
+					3:['data.txt'], 
+					4:['data_minimized.txt'],
+					5:['data_init.txt','ScriptGroup.0.txt'], #--- only one partition! for multiple ones, use 'submit.py'
+				 }[0] #--- to be copied from the above directory
+	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
-	MEAM_library_DIR='/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles' #--- meam potential parameters
-	PYFIL = '/home/kamran.karimi1/Project/git/CrystalPlasticity/py'
-	EXEC = 'lmp_gpu' #'lmp_mpi' #'lmp_serial'
-	durtn = '00:15:00' #'167:59:59'
+	#
+	MEAM_library_DIR='/home/kamran.karimi1/Project/git/lammps2nd/lammps/potentials'
+	#
+	SCRPT_DIR = os.getcwd()+'/lmpScripts/'+{1:'cuzr'}[1]
+	#
 	SCRATCH = None
+	OUT_PATH = '.'
+	if SCRATCH:
+		OUT_PATH = '/scratch/${SLURM_JOB_ID}'
+	#--- py script must have a key of type str!
+	LmpScript = {	0:'in.melt',
+				 	1:'relax.in', 
+					2:'relaxWalls.in', 
+					7:'in.Thermalization', 
+					4:'in.vsgc', 
+					5:'in.minimization', 
+					6:'in.shearDispTemp', 
+					8:'in.shearLoadTemp',
+					9:'in.elastic',
+					10:'in.elasticSoftWall',
+					'p0':'partition.py',
+					'p1':'WriteDump.py',
+					'p2':'DislocateEdge.py',
+				} 
+	#
+	Variable = {
+				0:' -var natoms 1000 -var cutoff 3.52 -var tstart 0.01 -var tstop 2000 -var ParseData 0  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
+				6:' -var T 300 -var DataFile Equilibrated_300.dat',
+				4:' -var T 600 -var t_sw 20.0 -var DataFile Equilibrated_600.dat -var nevery 1000 -var ParseData 1 -var WriteData swapped_600.dat', 
+				5:' -var DataFile data.txt -var buff 3.0 -var DumpFile dumpMin.xyz -var nevery 1000 -var ParseData 1 -var WriteData data_minimized.txt', 
+				7:' -var buff 3.0 -var T 600 -var teq 200.0 -var nevery 1000 -var ParseData 1 -var DataFile data_init.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_600.dat',
+				8:' -var buff 3.0 -var T 0.1 -var sigm 1.5 -var sigmdt 0.01 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
+				9:' -var natoms 1000 -var cutoff 3.52 -var ParseData 1',
+				10:' -var ParseData 1 -var DataFile swapped_600.dat',
+				'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
+				'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
+				'p2':' %s 3.52 40.0 20.0 40.0 data.txt'%(os.getcwd()+'/../postprocess'),
+				} 
+	#--- different scripts in a pipeline
+	indices = {
+				1:[0], #--- melt & quench
+
+			  }[1]
+	Pipeline = list(map(lambda x:LmpScript[x],indices))
+	Variables = list(map(lambda x:Variable[x], indices))
+	EXEC = list(map(lambda x:'lmp' if type(x) == type(0) else 'py', indices))	
+	#
+	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
+	durtn = ['23:59:59','00:59:59'][0]
 	mem = '8gb'
-	partition = ['gpu-v100','parallel','cpu2019','single'][0]
-	#--- sim. parameters
-	natom = 50000  
-	Tfinal = 3000 #--- melt. temp.	 
-	ntypes = 5
-        cutoff = 3.58
-    #   cutoffs = np.linspace((1.0-0.5)*cutoff,(1+0.5)*cutoff,nruns)
-#	rho = 0.1
+	partition = ['gpu-v100','parallel','cpu2019','single'][1]
 	#---
 	os.system( 'rm -rf %s' % jobname ) #--- rm existing
 	os.system( 'rm jobID.txt' )
@@ -66,13 +107,16 @@ if __name__ == '__main__':
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
 		if irun == 0: #--- cp to directory
 			path=os.getcwd() + '/%s' % ( jobname)
-			os.system( 'cp %s/%s %s' % ( EXEC_DIR, EXEC, path ) ) # --- create folder & mv oar scrip & cp executable
+			os.system( 'ln -s %s/%s %s' % ( EXEC_DIR, EXEC_lmp, path ) ) # --- create folder & mv oar scrip & cp executable
 		#---
-		os.system( 'cp in_melt.txt %s/in.txt ' % writPath ) #--- lammps script: periodic x, pxx, vy, load
-#		os.system( 'cp in_test.txt %s/in.txt ' % writPath ) #--- lammps script: periodic x, pxx, vy, load
+		for script,indx in zip(Pipeline,range(100)):
+#			os.system( 'cp %s/%s %s/lmpScript%s.txt' %( SCRPT_DIR, script, writPath, indx) ) #--- lammps script: periodic x, pxx, vy, load
+			os.system( 'ln -s %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+		if sourceFiles: 
+			for sf in sourceFiles:
+				os.system( 'ln -s %s/Run%s/%s %s' %(sourcePath, irun, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		#---
-		#---
-		makeOAR( path, 1, nThreads, durtn, PYFIL ) # --- make oar script
+		makeOAR( path, 1, nThreads, durtn) # --- make oar script
 		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s' % ( writPath) ) # --- create folder & mv oar scrip & cp executable
 		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
 						    --chdir %s -c %s -n %s %s/oarScript.sh >> jobID.txt'\
