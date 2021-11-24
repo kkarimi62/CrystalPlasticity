0bbb92779396e9bb42d3d0709ef0f194a0619464
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-11-24 11:42:31 +0100

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
 	nruns	 = 1
 	#
-	nThreads = [1,9][0]
+	nThreads = [1,9][1]
 	nNode	 = 1
 	#
 	jobname  = {
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
 	durtn = ['96:59:59','00:59:59'][1]
 	mem = '8gb'
-	partition = ['gpu-v100','parallel','cpu2019','single'][1]
+	partition = ['gpu-v100','parallel','cpu2019','single'][2]
 	#---
 	os.system( 'rm -rf %s' % jobname ) #--- rm existing
 	os.system( 'rm jobID.txt' )
