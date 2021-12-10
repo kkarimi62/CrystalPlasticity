48fac8365a04ef7925ccff2681c97a9288eccf3d
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-10 21:36:25 +0100

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -96,7 +96,7 @@ if __name__ == '__main__':
 	EXEC = list(map(lambda x:'lmp' if type(x) == type(0) else 'py', indices))	
 	#
 	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
-	durtn = ['96:59:59','23:59:59'][1]
+	durtn = ['96:59:59','00:59:59'][1]
 	mem = '8gb'
 	partition = ['gpu-v100','parallel','cpu2019','single'][1]
 	#---
