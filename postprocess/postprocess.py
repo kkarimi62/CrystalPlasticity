1a8d230233fe8ba77f059a39181f07a380ff1d45
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-10 09:56:25 +0100

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -15,15 +15,15 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = range(3)
-	jobname  = 'd2minRatioCo5Cr2Fe40Mn27Ni26' 
+	jobname  = 'FeNiT300Elasticity' 
 	DeleteExistingFolder = True
-	readPath = os.getcwd() + '/../testRuns/glassCo5Cr2Fe40Mn27Ni26' #--- source
+	readPath = os.getcwd() + '/../testRuns/Preparation/FeNiT300Elasticity' #--- source
 	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '23:59:59'
+	durtn = '00:59:59'
 	mem = '16gb'
 	partition = ['parallel','cpu2019','bigmem','single'][2] 
 	argv = "path=%s"%(readPath) #--- don't change! 
-	argv2nd = "indx=5" 
+	argv2nd = "indx=0" 
 	PYFILdic = { 
 		0:'ElasticConstants.ipynb',
 		1:'analyzePlasticity.ipynb',
