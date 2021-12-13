dc985049992a64a26933992a1a2792b8ef994fe1
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-13 16:48:17 +0100

    update

--- testRuns/Preparation/submitPY.py
+++ testRuns/Preparation/submitPY.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
 #	nn = 4
 #	NTHRESH = np.linspace(0.05,0.11,nn,endpoint=True)
 	#---
-	jobname = 'CoNiFeT300Elasticity' #'testCpuRunTime'
+	jobname = 'CoNiCrFeT300Elasticity' #'testCpuRunTime'
 
 #	PHI = [[PHI[iphi],NTHRESH[inn]] for iphi in xrange( nphi ) for inn in xrange(nn)]
 #	nphi = len(PHI)
