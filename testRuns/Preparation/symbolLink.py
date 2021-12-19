1f03dc561b7803e56505a901c9a7019545403f82
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-19 17:06:22 +0100

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -4,12 +4,12 @@ if __name__ == '__main__':
 	import os
 	import sys
 	#--- 
-	jobname  = 'uniax17'
-	job_id = 9358490
+	jobname  = 'CoNiCrFeT300Elasticity0'
+	job_id = 12158618 
 	N = 3
 	#---
 	job_ids = [ job_id + i for i in xrange( N ) ]
 	for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-		for file_name in [ 'cap_stress.dump', 'pair_stress.dump' ]:
+		for file_name in [ 'dump_init.xyz', 'dump_def6.xyz' ]:
 			os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
