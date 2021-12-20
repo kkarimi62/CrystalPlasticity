b46f9cf0383a29382e96ca70a8dcbd3d153d9ab4
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-20 09:34:57 +0100

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 	mem = '16gb'
 	partition = ['parallel','cpu2019','bigmem','single'][2] 
 	argv = "path=%s"%(readPath) #--- don't change! 
-	argv2nd = "itime=0\nindx=0" 
+	argv2nd = "itime=0\nindx=1" 
 	PYFILdic = { 
 		0:'ElasticConstants.ipynb',
 		1:'analyzePlasticity.ipynb',
