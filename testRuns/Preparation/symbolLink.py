84606059945c0eb359c91afd1f34d5fa3554f199
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-21 10:43:49 +0100

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -13,11 +13,11 @@ if __name__ == '__main__':
              'Co5Cr2Fe40Mn27Ni26'
          ] 
    
-	for loopi in PHI: 
-		for loop in range(0,1,1):
+	for loopi in PHI: #--- composition 
+		for loop in range(0,1,1): #--- itimes
 			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
 			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
-			N = 3
+			N = 3 #--- nruns
 	#---
 			job_ids = [ job_id + i for i in xrange( N ) ]
 			for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
