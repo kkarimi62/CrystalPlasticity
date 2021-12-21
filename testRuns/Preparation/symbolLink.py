75f2f9b047ba9778b2d0338cde30008991ed290d
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-12-21 10:38:30 +0100

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -12,14 +12,15 @@ if __name__ == '__main__':
              'CoNiCrFeMn',
              'Co5Cr2Fe40Mn27Ni26'
          ] 
-    for loopi in PHI: 
-        for loop in range(0,1,1):
-            jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
-            job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
-            N = 3
+   
+	for loopi in PHI: 
+		for loop in range(0,1,1):
+			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
+			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
+			N = 3
 	#---
-	    job_ids = [ job_id + i for i in xrange( N ) ]
-	    for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
+		job_ids = [ job_id + i for i in xrange( N ) ]
+		for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
 		for file_name in [ 'dump_init.xyz', 'dump_def6.xyz' ]:
 			os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
