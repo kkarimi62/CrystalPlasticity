7a1feb2b3d491199b53bcea899c3e35ce8166ddc
kkarimi62 <kamran.karimi1362@gmail.com>
2022-01-10 11:59:04 +0100

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
 					3:['data.0.txt','CoNiCrFe_glass.dump','CoNiCrFe.txt'], 
 					4:['data_minimized.txt'],
 					5:['data_init.txt','ScriptGroup.0.txt'], #--- only one partition! for multiple ones, use 'submit.py'
-					6:['data.1000000.txt','dumpSheared.xyz'], 
+					6:['data.100.txt','dumpSheared.xyz'], 
 				 }[6] #--- to be copied from the above directory
 	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 				7:' -var buff 3.0 -var T 600 -var teq 200.0 -var nevery 1000 -var ParseData 1 -var DataFile data_init.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_600.dat',
 				8:' -var buff 3.0 -var T 0.1 -var sigm 1.5 -var sigmdt 0.01 -var ParseData 1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
 				9:' -var natoms 1000 -var cutoff 3.52 -var ParseData 1',
-				10:' -var T 300.0 -var teq	2.0	-var nevery 100 -var ParseData 1 -var DataFile data.1000000.txt',
+				10:' -var T 300.0 -var teq	2.0	-var nevery 100 -var ParseData 1 -var DataFile data.100.txt',
 				'p0':' swapped_600.dat 10.0 %s'%(os.getcwd()+'/../postprocess'),
 				'p1':' swapped_600.dat ElasticConst.txt DumpFileModu.xyz %s'%(os.getcwd()+'/../postprocess'),
 				'p2':' %s 3.52 40.0 20.0 40.0 data.txt'%(os.getcwd()+'/../postprocess'),
