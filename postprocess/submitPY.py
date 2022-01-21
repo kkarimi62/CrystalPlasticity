5edb3335d681d4d0fd5f67f1c2b1f35c2d62ca72
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-01-21 16:03:23 +0100

    update

--- postprocess/submitPY.py
+++ postprocess/submitPY.py
@@ -8,13 +8,13 @@ if __name__ == '__main__':
 	string=open('postprocess.py').readlines() #--- python script
 	#---
 #	PHI=range(0,200,1)
-	PHI =[ 
-#            'FeNi',
-#            'CoNiFe',
-            'CoNiCrFe',
-#            'CoCrFeMn',
-#            'CoNiCrFeMn',
-#            'Co5Cr2Fe40Mn27Ni26'
+	PHI ={ 
+#            '0':'FeNi',
+#            '1':'CoNiFe',
+           '2':'CoNiCrFe',
+#           '3' :'CoCrFeMn',
+#            '4':'CoNiCrFeMn',
+#            '5':'Co5Cr2Fe40Mn27Ni26'
 		]
 	nphi = len(PHI)
 	#---
@@ -23,20 +23,20 @@ if __name__ == '__main__':
 #	PHI = [[PHI[iphi],NTHRESH[inn]] for iphi in xrange( nphi ) for inn in xrange(nn)]
 #	nphi = len(PHI)
 	for itime in times:
-		for iphi in range( nphi ):
+		for key in PHI:
 			#---	
 			inums = lnums[ 0 ] - 1
-			string[ inums ] = "\t\'1\':\'ElasticityT300/%s/itime%s',\n" % (PHI[iphi],itime) #--- change job name
+			string[ inums ] = "\t\'1\':\'ElasticityT300/%s/itime%s',\n" % (PHI[key],itime) #--- change job name
 #			string[ inums ] = "\t\'3\':\'PairCrltnT300/%s',\n" % (PHI[iphi]) #--- change job name
 	#		string[ inums ] = "\t\'4\':\'VorAnlT300/%s',\n" % (PHI[iphi]) #--- change job name
 	#		string[ inums ] = "\t\'5\':\'D2minAnalysisT300/%s',\n" % (PHI[iphi]) #--- change job name
 		#---	densities
 			inums = lnums[ 1 ] - 1
-			string[ inums ] = "\t\'1\':\'/../testRuns/Preparation/ElasticityT300/%s/itime%s\',\n"%(PHI[iphi],itime)
+			string[ inums ] = "\t\'1\':\'/../testRuns/Preparation/ElasticityT300/%s/itime%s\',\n"%(PHI[key],itime)
 #			string[ inums ] = "\t\'2\':\'/../testRuns/glass%s\',\n"%(PHI[iphi])
 	#
 			inums = lnums[ 2 ] - 1
-			string[ inums ] = "\targv2nd = \'itime=%s\\nindx=%s\'\n"%(itime*10000,iphi) #(PHI[iphi]*10000)
+			string[ inums ] = "\targv2nd = \'itime=%s\\nindx=%s\'\n"%(itime*10000,key) #(PHI[iphi]*10000)
 
 			sfile=open('junk%s.py'%iphi,'w');sfile.writelines(string);sfile.close()
 			os.system( 'python3 junk%s.py'%iphi )
