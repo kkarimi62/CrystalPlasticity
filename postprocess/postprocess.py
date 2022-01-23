c0bf81190214c25c53b343fb6676e208e6a6a848
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-01-23 09:15:07 +0100

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -29,7 +29,7 @@ if __name__ == '__main__':
  							}['1'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:59:59'
-	mem = '32gb'
+	mem = '16gb'
 	partition = ['parallel','cpu2019','bigmem','single'][2] 
 	argv = "path=%s"%(readPath) #--- don't change! 
 	argv2nd = "itime=2000000\nindx=5" 
