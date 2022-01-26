63ca4e22f814b7ed369ee32627942b3bae25dbac
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-01-26 14:48:56 +0100

    update

--- postprocess/junk.py
+++ postprocess/junk.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 filee={1:'negative_mu',2:'mapCxyxy'}[1]
 glass={    '0':'FeNi',
@@ -8,7 +9,7 @@ glass={    '0':'FeNi',
            '4':'CoNiCrFeMn',
            '5':'Co5Cr2Fe40Mn27Ni26'
 	  }['2']
-irun=1
-for i in range(0,208,8): #[0,50,101,120,195]: #range(0,208,8):
+irun=0
+for i in np.arange(0,200+1,1): #range(0,208,8): #[0,50,101,120,195]: #range(0,208,8):
 	print('ElasticityT300/%s/itime%i/Run%s/%s.%i.png'%(glass,i,irun,filee,i*10000))
 #	os.system('cp ElasticityT300/%s/itime%i/Run%s/%s.%s.png %s.%s.png'%(glass,i,irun,filee,i*10000,filee,i))
