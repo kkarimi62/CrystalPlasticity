47cf5c0dbfe2b489d0e459969f2e62bee0535667
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-01-26 13:35:48 +0100

    update

--- postprocess/junk.py
+++ postprocess/junk.py
@@ -1,6 +1,14 @@
 import os
 
 filee={1:'negative_mu',2:'mapCxyxy'}[1]
-for i in [0,50,101,120,195]: #range(0,208,8):
-	print('ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime%i/Run0/%s.%i.png'%(i,filee,i*10000))
-#	os.system('cp ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime%i/Run0/%s.%s.png %s.%s.png'%(i,filee,i*10000,filee,i))
+glass={    '0':'FeNi',
+           '1':'CoNiFe',
+           '2':'CoNiCrFe',
+           '3' :'CoCrFeMn',
+           '4':'CoNiCrFeMn',
+           '5':'Co5Cr2Fe40Mn27Ni26'
+	  }['2']
+irun=1
+for i in range(0,208,8): #[0,50,101,120,195]: #range(0,208,8):
+	print('ElasticityT300/%s/itime%i/Run%s/%s.%i.png'%(glass,i,irun,filee,i*10000))
+#	os.system('cp ElasticityT300/%s/itime%i/Run%s/%s.%s.png %s.%s.png'%(glass,i,irun,filee,i*10000,filee,i))
