ffb8e9196d1b7aeec0275e222601f452042d93a9
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-02-03 16:58:45 +0100

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -8,18 +8,19 @@ if __name__ == '__main__':
 	PHI = [  
              'FeNi',
              'CoNiFe',
-#             'CoNiCrFe',
+             'CoNiCrFe',
              'CoCrFeMn',
              'CoNiCrFeMn',
-#             'Co5Cr2Fe40Mn27Ni26'
+             'Co5Cr2Fe40Mn27Ni26'
          ] 
    
-	N = 1 #--- nruns
+	runs=[1] #[0,1,2] #--- nruns
 	for loopi in PHI: #--- composition 
 		for loop in np.arange(0,200+1,1): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
 			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
 			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
 	#---
+			N = len(runs)
 			job_ids = [ job_id + i for i in xrange( N ) ]
 			for id_job, counter in zip( job_ids, xrange( sys.maxint ) ):
 				writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
