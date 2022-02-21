4a18e18fb0c46ec9aceb366947038df09ccc6a55
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-02-21 11:30:52 +0100

    update

--- postprocess/junk.py
+++ postprocess/junk.py
@@ -8,8 +8,13 @@ glass={    '0':'FeNi',
            '3' :'CoCrFeMn',
            '4':'CoNiCrFeMn',
            '5':'Co5Cr2Fe40Mn27Ni26'
-	  }['2']
-irun=0
-for i in [0,50,101,120,195]: #np.arange(0,200+1,1): #range(0,208,8): #[0,50,101,120,195]: #range(0,208,8):
+	  }['5']
+#irun=0
+for irun in [0,1,2]:
+	for itime in np.arange(0,200+1,2): #[0,50,101,120,195]: #np.arange(0,200+1,1): #range(0,208,8): #[0,50,101,120,195]: #range(0,208,8):
+	
 #	print('ElasticityT300/%s/itime%i/Run%s/%s.%i.png'%(glass,i,irun,filee,i*10000))
-	os.system('cp ElasticityT300/%s/itime%i/Run%s/%s.%s.png %s.%s.png'%(glass,i,irun,filee,i*10000,filee,i))
+#	os.system('cp ElasticityT300/%s/itime%i/Run%s/%s.%s.png %s.%s.png'%(glass,i,irun,filee,i*10000,filee,i))
+		os.system('cp ModuAnl/xlin.txt ElasticityT300/%s/itime%i/Run%s/ModuAnl'%(glass,itime,irun))
+		os.system('cp ModuAnl/ylin.txt ElasticityT300/%s/itime%i/Run%s/ModuAnl'%(glass,itime,irun))
+		os.system('cp ModuAnl/zlin.txt ElasticityT300/%s/itime%i/Run%s/ModuAnl'%(glass,itime,irun))
