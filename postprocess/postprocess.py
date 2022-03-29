48701c28a78dadebd60efafcf39ce436e682b87d
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-03-29 13:20:29 +0200

    updates

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -14,25 +14,25 @@ def makeOAR( EXEC_DIR, node, core, tpartitionime, PYFIL, argv,argv2nd):
 if __name__ == '__main__':
 	import os
 #
-	runs	 = [2] #[0]#,1,2]
+	runs	 = [0,1,2]
 	jobname  = {
 				'1':'ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime0', 
-				'2':'MlTrain/Co5Cr2Fe40Mn27Ni26', 
+				'2':'MlTrain/Co5Cr2Fe40Mn27Ni26/itime0', 
 				'3':'PairCrltnT300/Co5Cr2Fe40Mn27Ni26', 
 				'4':'VorAnlT300/Co5Cr2Fe40Mn27Ni26', 
 				'5':'D2minAnalysisT300/Co5Cr2Fe40Mn27Ni26', 
-				}['1']
+				}['2']
 	DeleteExistingFolder = False
 	readPath = os.getcwd() + {
 								'1':'/../testRuns/Preparation/ElasticityT300/Co5Cr2Fe40Mn27Ni26/itime0',
 								'2':'/../testRuns/glassCo5Cr2Fe40Mn27Ni26',
- 							}['1'] #--- source
+ 							}['2'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '00:59:59'
-	mem = '10gb'
-	partition = ['parallel','cpu2019','bigmem','single'][3] 
+	durtn = '23:59:59'
+	mem = '64gb'
+	partition = ['parallel','cpu2019','bigmem','single'][2] 
 	argv = "path=%s"%(readPath) #--- don't change! 
-	argv2nd = "itime=2000000\nindx=5" 
+	argv2nd = "itime=0\nindx=5" 
 	PYFILdic = { 
 		0:'ElasticConstants.ipynb',
 		1:'analyzePlasticity.ipynb',
