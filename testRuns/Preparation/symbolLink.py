ee1311adf6d958ae6f8c9bf01f91511ff847b69f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-04-30 11:43:44 +0200

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -12,7 +12,7 @@ if __name__ == '__main__':
 #             'CoCrFeMn',
 #             'CoNiCrFeMn',
 #             'Co5Cr2Fe40Mn27Ni26',
-			  'CuZr2'	
+			  'CuZr3'	
          ] 
    
 	runs=[0] #[0,1,2] #--- nruns
