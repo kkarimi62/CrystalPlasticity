f2ce2ec6d71b0f96c0d0c9a5efdc3d70da6f3cb9
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-03 11:27:42 +0200

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -11,13 +11,13 @@ if __name__ == '__main__':
 #             'CoNiCrFe',
 #             'CoCrFeMn',
 #             'CoNiCrFeMn',
-#             'Co5Cr2Fe40Mn27Ni26',
-			  'CuZr3'	
+             'Co5Cr2Fe40Mn27Ni26',
+#			  'CuZr3'	
          ] 
    
-	runs=[0] #[0,1,2] #--- nruns
+	runs=[0,1,2] #--- nruns
 	for loopi in PHI: #--- composition 
-		for loop in np.arange(0,100+1,1): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
+		for loop in np.arange(0,200+1,4): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
 			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
 			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
 	#---
