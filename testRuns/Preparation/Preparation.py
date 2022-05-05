e7509f6fdb6d386b4390704ee32446d718d2e1ce
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-05 21:43:07 +0200

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -103,7 +103,7 @@ if __name__ == '__main__':
 	DeleteExistingFolder = True
 	#
 	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
-	durtn = ['96:59:59','00:59:59'][1]
+	durtn = ['96:59:59','03:59:59'][1]
 	mem = '8gb'
 	partition = ['gpu-v100','parallel','cpu2019','single'][1]
 	#---
