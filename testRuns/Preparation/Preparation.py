9fbf73b3042aad6e3433ec557084d3d9939781fe
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-05 12:31:21 +0200

    update

--- testRuns/Preparation/Preparation.py
+++ testRuns/Preparation/Preparation.py
@@ -123,7 +123,7 @@ if __name__ == '__main__':
 		#---
 		for script,indx in zip(Pipeline,range(100)):
 #			os.system( 'cp %s/%s %s/lmpScript%s.txt' %( SCRPT_DIR, script, writPath, indx) ) #--- lammps script: periodic x, pxx, vy, load
-			os.system( 'cp %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+			os.system( 'ln -s %s/%s %s' %( SCRPT_DIR, script, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		if sourceFiles: 
 			for sf in sourceFiles:
 				os.system( 'ln -s %s/Run%s/%s %s' %(sourcePath, counter, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -14,17 +14,24 @@ if __name__ == '__main__':
              'Co5Cr2Fe40Mn27Ni26',
 #			  'CuZr3'	
          ] 
+
+	EPS = { '0':0.01,
+    	    '1':0.1,
+        	'2':1.0
+		  }
+	
    
 	runs=[0] #,1,2] #--- nruns
 	for loopi in PHI: #--- composition 
-		for loop in np.arange(0,200+1,4): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
-			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
-			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
+		for epsi in EPS: #--- strain 
+			for loop in np.arange(0,200+1,4): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
+				jobname  = 'ElasticityT300/%s/eps%s/itime%s'%(loopi,epsi,loop)
+				job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
 	#---
-			N = len(runs)
-			job_ids = [ job_id + i for i in xrange( N ) ]
-			assert len(job_ids) == len(runs)
-			for id_job, counter in zip( job_ids, runs): #xrange( sys.maxint ) ):
-				writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-				for file_name in ['shearOscillation.xyz']: #[ 'dump_init.xyz', 'dump_def6.xyz' ]:
-					os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
+				N = len(runs)
+				job_ids = [ job_id + i for i in xrange( N ) ]
+				assert len(job_ids) == len(runs)
+				for id_job, counter in zip( job_ids, runs): #xrange( sys.maxint ) ):
+					writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
+					for file_name in ['shearOscillation.xyz']: #[ 'dump_init.xyz', 'dump_def6.xyz' ]:
+						os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
