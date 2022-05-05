7d3d4a1a1c956470daf754c0bcea31122b0f57e6
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-05 09:05:38 +0200

    update

--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -15,10 +15,10 @@ if __name__ == '__main__':
 #			  'CuZr3'	
          ] 
    
-	runs=[0,1,2] #--- nruns
+	runs=[0] #,1,2] #--- nruns
 	for loopi in PHI: #--- composition 
 		for loop in np.arange(0,200+1,4): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
-			jobname  = 'ElasticityT300/%s/eps0/itime%s'%(loopi,loop)
+			jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
 			job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
 	#---
 			N = len(runs)
@@ -26,5 +26,5 @@ if __name__ == '__main__':
 			assert len(job_ids) == len(runs)
 			for id_job, counter in zip( job_ids, runs): #xrange( sys.maxint ) ):
 				writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
-				for file_name in [ 'dump_init.xyz', 'dump_def6.xyz' ]:
+				for file_name in ['shearOscillation.xyz']: #[ 'dump_init.xyz', 'dump_def6.xyz' ]:
 					os.system( 'ln -s /scratch/%s/%s %s/' % ( id_job, file_name, writPath ) )
