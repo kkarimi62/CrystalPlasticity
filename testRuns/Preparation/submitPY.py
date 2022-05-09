056e4a98d0ccf030b11c5bdeb9be64e0e8766c8c
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-09 17:33:49 +0200

    update

--- testRuns/Preparation/submitPY.py
+++ testRuns/Preparation/submitPY.py
@@ -27,8 +27,8 @@ if __name__ == '__main__':
 	EPS = { 
 #			'0':1.0e-03,
 #			'1':0.5e-03,
-#			'2':0.25e-03,
-			'3':2.0e-02,
+			'2':0.25e-03,
+#			'3':2.0e-02,
 #			'4':16.0,
 #			'5':32.0,
 #			'6':64.0,
@@ -39,7 +39,7 @@ if __name__ == '__main__':
 #	nn = 4
 #	NTHRESH = np.linspace(0.05,0.11,nn,endpoint=True)
 	#---
-	times=np.arange(0,200+1,4) #np.arange(0,200+1,2) 
+	times=np.arange(0,200+1,1) #np.arange(0,200+1,2) 
 	#--- 
 	count = 0
 	for iphi in PHI:
--- testRuns/Preparation/symbolLink.py
+++ testRuns/Preparation/symbolLink.py
@@ -18,18 +18,18 @@ if __name__ == '__main__':
 	EPS = { 
 #			'0':1.0e-03,
 #			'1':0.5e-03,
-#			'2':0.25e-03,
-            '3':2.0e-02,
+			'2':0.25e-03,
+#            '3':2.0e-02,
 #			'4':16.0,
 #			'5':32.0,
 #			'6':64.0,
 		  }
 	
    
-	runs=[0] #,1,2] #--- nruns
+	runs=[0,1,2] #--- nruns
 	for loopi in PHI: #--- composition 
 		for epsi in EPS: #--- strain 
-			for loop in np.arange(0,200+1,4): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
+			for loop in np.arange(0,200+1,1): #[50,101,195]: #np.arange(0,200+8,8): #--- itimes
 				jobname  = 'ElasticityT300/%s/eps%s/itime%s'%(loopi,epsi,loop)
 #				jobname  = 'ElasticityT300/%s/itime%s'%(loopi,loop)
 				job_id = int(open('%s/jobID.txt'%jobname).readlines()[0].split()[3])
