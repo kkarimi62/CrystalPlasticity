f013ed91ef64d36512cf55db260e262b25d005ca
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-16 18:12:53 +0200

    update

--- postprocess/submitPY.py
+++ postprocess/submitPY.py
@@ -9,10 +9,10 @@ if __name__ == '__main__':
 	#---
 	PHI ={ 
 #            '0':'FeNi',
-            '1':'CoNiFe',
+#            '1':'CoNiFe',
 #           '2':'CoNiCrFe',
 #           '3' :'CoCrFeMn',
-#            '4':'CoNiCrFeMn',
+            '4':'CoNiCrFeMn',
 #            '5':'Co5Cr2Fe40Mn27Ni26'
 #			'6':'CuZr3',
 		}
