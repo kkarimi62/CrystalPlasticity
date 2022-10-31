4abd19e796b3ec860ded7997e874cd3cfd4df75e
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-10-31 14:00:39 +0100

    updates

--- testRuns/Preparation/Preparation_ncbj.py
+++ testRuns/Preparation/Preparation_ncbj.py
@@ -4,7 +4,10 @@ def makeOAR( EXEC_DIR, node, core, time ):
 	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
 	print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
 #	print >> someFile, 'module load mpich/3.2.1-gnu\n'
-	print >> someFile, 'source /mnt/opt/spack-0.17/share/spack/setup-env.sh\nspack load openmpi@4.0.5 %gcc@9.3.0\nspack load openblas@0.3.18%gcc@9.3.0\nspack load python@3.8.12%gcc@8.3.0\n\n',
+	print >> someFile, 'source /mnt/opt/spack-0.17/share/spack/setup-env.sh\n\n'
+#						spack load openmpi@4.0.5 %gcc@9.3.0\n\
+#						spack load openblas@0.3.18%gcc@9.3.0\n\
+#						spack load python@3.8.12%gcc@8.3.0\n\n',
 	print >> someFile, 'export LD_LIBRARY_PATH=/mnt/opt/tools/cc7/lapack/3.5.0-x86_64-gcc46/lib:${LD_LIBRARY_PATH}\n'
 	#--- run python script 
 	for script,var,indx, execc in zip(Pipeline,Variables,range(100),EXEC):
